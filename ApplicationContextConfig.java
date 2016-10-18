1127915913bd54f4725f523885b7a6578ec1e36f
ANKITA MISHRA <mishrankita25@yahoo.com>
2016-10-18 23:50:49 +0530

    Add files via upload

--- ApplicationContextConfig.java
+++ ApplicationContextConfig.java
@@ -22,130 +22,147 @@ import com.niit.dao.EventDAO;
 import com.niit.dao.EventDAOImpl;
 import com.niit.dao.ForumDAO;
 import com.niit.dao.ForumDAOImpl;
+import com.niit.dao.FriendDAO;
+import com.niit.dao.FriendDAOImpl;
 import com.niit.dao.JobDAO;
 import com.niit.dao.JobDAOImpl;
 import com.niit.dao.UserDAO;
 import com.niit.dao.UserDAOImpl;
 import com.niit.model.BaseDomain;
 import com.niit.model.Blog;
-import com.niit.model.Blog_Comment;
+import com.niit.model.BlogComment;
 import com.niit.model.Chat;
 import com.niit.model.Event;
+import com.niit.model.EventComment;
 import com.niit.model.Forum;
-import com.niit.model.Forum_Comment;
+import com.niit.model.ForumComment;
 import com.niit.model.Friend;
 import com.niit.model.Job;
 import com.niit.model.User;
-
 
 @Configuration
 @ComponentScan("com.niit")
 @EnableTransactionManagement
 public class ApplicationContextConfig {
-	
-	@Bean(name = "dataSource")                               
+
+	@Bean(name = "dataSource")
 	public DataSource getOracleDataSource() {
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-	    dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
 
-	    dataSource.setUsername("ankita");
-	    dataSource.setPassword("anki25");
-	   
+		dataSource.setUsername("ankita");
+		dataSource.setPassword("anki25");
+		return dataSource;
+	}
 
-	 Properties connectionProperties=new Properties();
+	private Properties getHibernateProperties() {
 
-		  connectionProperties.setProperty("hibernate.show_sql", "true");   //to display the queries ,,,,can be given false if u want to hide the queries
-		  connectionProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
-		  connectionProperties.setProperty("hibernate.hbm2ddl.auto", "create");
-		  connectionProperties.setProperty("hibernate.jdbc.use_get_generated_keys","true");
-		  dataSource.setConnectionProperties(connectionProperties);
-		  return dataSource;
-		}
+		Properties connectionProperties = new Properties();
 
+		connectionProperties.setProperty("hibernate.show_sql", "true");
+		connectionProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+		connectionProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		connectionProperties.setProperty("hibernate.format_sql", "true");
+		connectionProperties.setProperty("hibernate.jdbc.use_get_generated_keys", "true");
+		// dataSource.setConnectionProperties(connectionProperties);
+		return connectionProperties;
+	}
 
+	@Autowired // automatically bean is created n injected
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
 
-		@Autowired   //automatically bean is created n injected
-		@Bean(name = "sessionFactory")
-		public SessionFactory getSessionFactory(DataSource dataSource)  {
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource); // sessionBuilder
+																								// can
+																								// b
+																								// any
+																								// other
+																								// name
+																								// too
+		sessionBuilder.addProperties(getHibernateProperties());
+		sessionBuilder.addAnnotatedClass(User.class);
+		sessionBuilder.addAnnotatedClass(Blog.class);
+		sessionBuilder.addAnnotatedClass(BaseDomain.class);
+		sessionBuilder.addAnnotatedClass(BlogComment.class);
+		// sessionBuilder.addAnnotatedClass(Chat.class);
+		sessionBuilder.addAnnotatedClass(Event.class);
+		sessionBuilder.addAnnotatedClass(EventComment.class);
+		sessionBuilder.addAnnotatedClass(ForumComment.class);
+		sessionBuilder.addAnnotatedClass(Forum.class);
+		sessionBuilder.addAnnotatedClass(Friend.class);
+		sessionBuilder.addAnnotatedClass(Job.class);
+		return sessionBuilder.buildSessionFactory();
+	}
 
-		  LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);  //sessionBuilder can b any other name too
-		 // sessionBuilder.addProperties(getHibernateProperties());
-		 sessionBuilder.addAnnotatedClass(User.class);
-		 sessionBuilder.addAnnotatedClass(Blog.class);
-		 sessionBuilder.addAnnotatedClass(BaseDomain.class);
-		 sessionBuilder.addAnnotatedClass(Blog_Comment.class);
-		 //sessionBuilder.addAnnotatedClass(Chat.class);
-		 //sessionBuilder.addAnnotatedClass(Event.class);
-		// sessionBuilder.addAnnotatedClass(Forum_Comment.class);
-		// sessionBuilder.addAnnotatedClass(Forum.class);
-		 //sessionBuilder.addAnnotatedClass(Friend.class);
-		// sessionBuilder.addAnnotatedClass(Job.class);
-		  return sessionBuilder.buildSessionFactory();
-		}
-
-		
-		@Autowired
-		@Bean(name = "transactionManager")
-		public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);  //should be same name as bean name of session factory
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory); // should
+																											// be
+																											// same
+																											// name
+																											// as
+																											// bean
+																											// name
+																											// of
+																											// session
+																											// factory
 		return transactionManager;
-		}
-		
-		@Autowired
-		@Bean(name = "user")
-            public User getUser (){
-			return new User();
-		}
-		
-		
-		@Autowired
-		@Bean(name = "userDAO")
-            public UserDAO getUserDAO (SessionFactory sessionFactory){
-			return new UserDAOImpl(sessionFactory);
-		}
-		
+	}
 
-		@Autowired
-		@Bean(name = "blog")
-            public Blog getBlog (){
-			return new Blog();
-		}
-		
-		
-		@Autowired
-		@Bean(name = "blogDAO")
-            public BlogDAO getBlogDAO (SessionFactory sessionFactory){
-			return new BlogDAOImpl(sessionFactory);
-		}
-		
-		@Autowired
-		@Bean(name = "jobDAO")
-            public JobDAO getJobDAO (SessionFactory sessionFactory){
-			return new JobDAOImpl(sessionFactory);
-		}
+	@Autowired
+	@Bean(name = "user")
+	public User getUser() {
+		return new User();
+	}
 
-		@Autowired
-		@Bean(name = "chatDAO")
-            public ChatDAO getChatDAO (SessionFactory sessionFactory){
-			return new ChatDAOImpl(sessionFactory);
-		}
+	@Autowired
+	@Bean(name = "userDAO")
+	public UserDAO getUserDAO(SessionFactory sessionFactory) {
+		return new UserDAOImpl(sessionFactory);
+	}
 
-		@Autowired
-		@Bean(name = "eventDAO")
-            public EventDAO getEventDAO (SessionFactory sessionFactory){
-			return new EventDAOImpl(sessionFactory);
-		}
+	@Autowired
+	@Bean(name = "blog")
+	public Blog getBlog() {
+		return new Blog();
+	}
 
-		@Autowired
-		@Bean(name = "forumDAO")
-            public ForumDAO getForumDAO (SessionFactory sessionFactory){
-			return new ForumDAOImpl(sessionFactory);
-		}
+	@Autowired
+	@Bean(name = "blogDAO")
+	public BlogDAO getBlogDAO(SessionFactory sessionFactory) {
+		return new BlogDAOImpl(sessionFactory);
+	}
+
+	@Autowired
+	@Bean(name = "jobDAO")
+	public JobDAO getJobDAO(SessionFactory sessionFactory) {
+		return new JobDAOImpl(sessionFactory);
+	}
+
+	@Autowired
+	@Bean(name = "chatDAO")
+	public ChatDAO getChatDAO(SessionFactory sessionFactory) {
+		return new ChatDAOImpl(sessionFactory);
+	}
+
+	@Autowired
+	@Bean(name = "eventDAO")
+	public EventDAO getEventDAO(SessionFactory sessionFactory) {
+		return new EventDAOImpl(sessionFactory);
+	}
+
+	@Autowired
+	@Bean(name = "forumDAO")
+	public ForumDAO getForumDAO(SessionFactory sessionFactory) {
+		return new ForumDAOImpl(sessionFactory);
+	}
+	
+	@Autowired
+	@Bean(name = "friendDAO")
+	public FriendDAO getFriendDAO(SessionFactory sessionFactory) {
+		return new FriendDAOImpl(sessionFactory);
+	}
 
 }
-
-
-
-
-
